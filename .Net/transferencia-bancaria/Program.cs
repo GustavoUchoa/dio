c207595e33315a8af36e6063b471ea3b3943f740
GustavoUchoa <github@gustavouchoa.net>
2021-02-24 17:50:29 -0300

    criando esqueleto do projeto

---
+++ .Net/transferencia-bancaria/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace transferencia_bancaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
