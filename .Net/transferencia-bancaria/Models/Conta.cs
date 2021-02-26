73aeeb25e70b106ca3dd8e39762bc8181e37d1e3
GustavoUchoa <github@gustavouchoa.net>
2021-02-25 23:36:04 -0300

    adicionado funcionalidades: listar e criar contas

--- .Net/transferencia-bancaria/Models/Conta.cs
+++ .Net/transferencia-bancaria/Models/Conta.cs
@@ -26,22 +26,16 @@ namespace transferencia_bancaria.Models
             this.Credito = credito;
         }
 
+        public void Criar(TipoConta tipoConta, string nome, double saldo, double credito)
+        {
+            this.TipoConta = tipoConta;
+            this.Nome = nome;
+            this.Saldo = saldo;
+            this.Credito = credito;
+        }
+
         public void Sacar(Conta conta, double valorSaque)
         {
-            /*
-            if (this.Saldo - valorSaque < (this.Credito *-1))
-            {
-                Console.WriteLine("Saldo insuficiente!");
-                return false;
-            }
-            else
-            {
-                this.Saldo += valorSaque;
-                Console.WriteLine($"Saldo atual da conta de { this.Nome } é { this.Saldo }");
-                return true;
-            }
-            */
-
             if (SaldoSuficiente(conta, valorSaque))
                 Console.WriteLine($"Saldo atual da conta de { this.Nome } é { this.Saldo }");
             else
--- .Net/transferencia-bancaria/Program.cs
+++ .Net/transferencia-bancaria/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using transferencia_bancaria.Models;
+using transferencia_bancaria.Views;
+using transferencia_bancaria.Controllers;
 using transferencia_bancaria.Enum;
 
 namespace transferencia_bancaria
@@ -8,8 +9,71 @@ namespace transferencia_bancaria
     {
         static void Main(string[] args)
         {
-            Conta conta = new Conta(TipoConta.PessoaFisica, "Gustavo", 1000, 100);
-            Console.WriteLine(conta.ToString());
+            /*string opcaoUsuario = Menu();
+            ExecutarOpcao(opcaoUsuario);*/
+
+            Conta novaConta = new Conta();
+            novaConta.Menu();
+            novaConta.ExecutarOpcao();
         }
+        /*
+        private static string Menu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("=============================");
+            Console.WriteLine("$$ Transferência Bancária $$");
+            Console.WriteLine("- Informe a opção desejada:");
+            Console.WriteLine("=============================");
+            
+            Console.WriteLine("1- Listar contas");
+            Console.WriteLine("2- Inserir nova conta");
+            Console.WriteLine("3- Transferir");
+            Console.WriteLine("4- Sacar");
+            Console.WriteLine("5- Depositar");
+            Console.WriteLine("C- Limpar tela");
+            Console.WriteLine("X- Sair");
+
+            string opcaoUsuario = Console.ReadLine();
+            return opcaoUsuario;
+        }
+
+        private static void ExecutarOpcao(string opcaoUsuario)
+        {
+            while (!opcaoUsuario.ToUpper().Equals("X"))
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        //ListarContas
+                        break;
+                    case "2":
+                        OpcaoInserir();
+                        break;
+                    
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+
+        private static void OpcaoInserir()
+        {
+            Console.Write("Escolha: 1- Conta Física |  2- Conta Jurídica: ");
+            int entradaTipoConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite seu nome: ");
+            string entradaNome = Console.ReadLine();
+
+            Console.Write("Digite seu saldo: ");
+            double entradaSaldo = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite seu crédito: ");
+            double entradaCredito = double.Parse(Console.ReadLine());
+
+            ContaController novaConta = new ContaController();
+            novaConta.InserirConta((TipoConta)entradaTipoConta, entradaNome, entradaSaldo, entradaCredito);
+            Console.WriteLine($"Conta criada: { novaConta.ToString() }");
+        }
+        */
     }
 }
